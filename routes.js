f4d7483fc20ef132cdd0b540d43fe67ae789729c
alcoimbra88 <al_coimbra@hotmail.com>
2021-08-26 18:48:58 -0300

    Criaçao do Cadatro de Contatos

--- routes.js
+++ routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const route = express.Router();
+
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController')
+const contatoController = require('./src/controllers/contatoController');
+
+const {loginRequired} = require('./src/middlewares/middleware');
 
 
 //Rotas da Home
@@ -12,5 +16,11 @@ route.get('/login/index', loginController.index);
 route.post('/login/register', loginController.register);
 route.post('/login/login', loginController.login);
 route.get('/login/logout', loginController.logout);
+
+
+//Rotas de Contato
+route.get('/contato/index',loginRequired,  contatoController.index);
+route.post('/contato/register', loginRequired,contatoController.register);
+route.get('/contato/index/:id', loginRequired,contatoController.editIndex);
 
 module.exports = route;
