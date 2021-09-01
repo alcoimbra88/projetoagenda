2ed92db2e8f2e3511be87fc2595e44a87502fc82
alan <al_coimbra@hotmail.com>
2021-09-01 19:10:21 -0300

    Validação de Login

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,10 @@
 const Contato = require('../models/ContatoModel')
 
 exports.index = async (req, res) => {
+
+  if(!req.session.user) return res.render('login');
+   
   const contatos = await Contato.buscaContatos();
   res.render('index', {contatos});
+
 };
