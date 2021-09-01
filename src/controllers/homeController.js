f08a16c749dec1a786d7476664ccfb065a2ff839
alan <al_coimbra@hotmail.com>
2021-09-01 19:21:01 -0300

    Commit teste validacao login

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,8 +2,8 @@ const Contato = require('../models/ContatoModel')
 
 exports.index = async (req, res) => {
 
-  if(!req.session.user) return res.render('login');
-   
+  if(!req.session.user) return res.redirect('login/index');
+
   const contatos = await Contato.buscaContatos();
   res.render('index', {contatos});
 
