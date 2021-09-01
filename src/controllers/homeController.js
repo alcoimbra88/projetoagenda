b5e2684867a0afa170fc83376a2900584200b248
alan <al_coimbra@hotmail.com>
2021-09-01 19:30:17 -0300

    mais um teste

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,9 +2,11 @@ const Contato = require('../models/ContatoModel')
 
 exports.index = async (req, res) => {
 
-  if(!req.session.user) return res.redirect('login/index');
-
+  if(!req.session.user) {
+    return res.redirect('login/index')
+  } else {
   const contatos = await Contato.buscaContatos();
   res.render('index', {contatos});
+}
 
 };
