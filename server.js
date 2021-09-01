dd58d5248113f71450b8832c2626c2864c9557e4
alan <al_coimbra@hotmail.com>
2021-09-01 18:30:52 -0300

    retorno da porta 3000

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use(checkCsrfError);
 app.use(csrfMiddleware);
 app.use(routes);
 app.on('pronto', () => {
-    app.listen(3001, () => {
+    app.listen(3000, () => {
         console.log('Acessar http://localhost:3000');
         console.log('Servidor Executando na porta 3000');
     });
